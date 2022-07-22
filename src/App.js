498eda488270d4573272ad11b0afb02a7bf08ef9
delanobmarques <delanobmarques@gmail.com>
2022-07-22 10:07:08 -0300

    add axios and react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ const App = () => {
   return (
     <div className="App">
       <h1>Address Book</h1>
+      <button className="btn btn-success m-5">
+        <i className="fa fa-home "/>
+        hello</button>
     </div>
   );
 }
